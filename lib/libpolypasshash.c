c312ebfdaae399e963ac4d7f4ec6a17e67a42745
Santiago Torres <torresariass@gmail.com>
2014-03-15 21:08:51 -0400

    Added empty function definitions

--- lib/libpolypasshash.c
+++ lib/libpolypasshash.c
@@ -25,7 +25,193 @@
 
 #include "config.h"
 #include "libgfshare.h"
+#include "libpolypasshash.h"
 
+
+/*******************************************************************
+* NAME :            pph_init_conext
+*
+* DESCRIPTION :     Initialize a poly pass hash structure with everything
+*                   we need in order to work. Custom initialization is to
+*                   be provided
+*
+* INPUTS :
+*   PARAMETERS:
+*     uint8 threshold:            he decided threshold for this specific
+*                                 password storage
+*
+*     const uint8 *secret:        the secret data provided to scramble the
+*                                 shamir secret share instance
+* 
+*     unsigned int secret_length: the length of the secret to use, should be 
+*                                 less than SHARE_LENGTH by preference
+*
+*      uint8 partial_bytes:       in case partial verification wants to be 
+*                                 disabled, a 0 will do
+* OUTPUTS :
+*   PARAMETERS:
+*     None
+*     
+*   GLOBALS :
+*     None
+*   
+*   RETURN :
+*     Type:   pph_context         the resulting context or NULL if something 
+*                                 fails
+* PROCESS :
+*     TODO: THIS
+*
+* CHANGES :
+*     TODO: 
+*/
+pph_context* pph_init_context(uint8 threshold, const uint8* secret,
+                              unsigned int secret_length, uint8 partial_bytes){
+  
+  // right now, we shouldn't be returning null
+  return NULL;
+}
+
+
+/*******************************************************************
+* NAME :            pph_create_account
+*
+* DESCRIPTION :     given a context and some other data, create a user
+*                   entry in the polypasshashcontext with the desired 
+*                   information.
+*
+* INPUTS :
+*   PARAMETERS:
+*     pph_context *ctx:      This is the context in which the account wiil be 
+*                            created
+*     
+*     const uint8 *username: This is the desired username for the new entry
+*
+*     const uint8 *password: This is the password for the new entry
+*
+*     uint8 shares:          This is the shares we decide to allocate to this
+*                            new account. 
+* OUTPUTS :
+*   PARAMETERS:
+*     None
+*     
+*   GLOBALS :
+*     None
+*   
+*   RETURN :
+*     Type: int PPH_ERROR     
+*           Values:        When:
+*           TODO: THIS              
+*           
+* PROCESS :
+*     TODO: THIS
+*
+* CHANGES :
+*     TODO: 
+*/
+PPH_ERROR pph_create_account(pph_context *ctx, const uint8 *username,
+                        const uint8 *password, uint8 shares){
+  EVP_MD_CTX mctx;
+   
+  return PPH_ERROR_UNKNOWN;
+}
+
+
+/*******************************************************************
+* NAME :          pph_check_log_in  
+*
+* DESCRIPTION :   Check whether a username and password combination exists 
+*                 inside the loaded PPH context
+*
+* INPUTS :
+*   PARAMETERS:
+*     pph_context *ctx:     The context in which we are working
+*
+*     const char *username: The username attempt
+*
+*     const char *password: The password attempt
+*
+* OUTPUTS :
+*   PARAMETERS:
+*     None
+*     
+*   GLOBALS :
+*     None
+*   
+*   RETURN :
+*     Type: int PPH_ERROR     
+*           Values:                         When:
+*           PPH_ACCOUNT_IS_INVALID            The combination does not exist
+*           
+*           PPH_USERNAME_IS_TOO_LONG          The username/pw won't fit in the
+*                                             context anyway
+*
+*           PPH_BAD_PTR                       When pointers are null or out
+*                                             of range
+*
+*           PPH_ERROR_UNKNOWN                 anytime else
+*           
+* PROCESS :
+*     TODO: THIS
+*
+* CHANGES :
+*     TODO: 
+*/
+PPH_ERROR pph_check_log_in(pph_context *ctx, const char *username, 
+                                                const char *password){
+  
+  return PPH_ERROR_UNKNOWN;
+}
+
+
+/*******************************************************************
+* NAME :          pph_unlock_password_data 
+*
+* DESCRIPTION :   given a context and pairs of usernames and passwords,
+*                 unlock the password secret. 
+*
+* INPUTS :
+*   PARAMETERS:
+*     pph_context *ctx:      The context in which we are working
+*
+*     uint8 share_number:    The length of the username/password pair arrays
+*
+*     const char *usernames: The username attempts
+*
+*     const char *passwords: The password attempts
+*
+* OUTPUTS :
+*   PARAMETERS:
+*     None
+*     
+*   GLOBALS :
+*     None
+*   
+*   RETURN :
+*     Type: int PPH_ERROR     
+*           Values:                         When:
+*           PPH_ACCOUNT_IS_INVALID            We couln't recombine with the 
+*                                             information given
+*           
+*           PPH_USERNAME_IS_TOO_LONG          The username/pw won't fit in the
+*                                             context anyway
+*
+*           PPH_BAD_PTR                       When pointers are null or out
+*                                             of range
+*
+*           PPH_ERROR_UNKNOWN                 anytime else
+*           
+* PROCESS :
+*     TODO: THIS
+*
+* CHANGES :
+*     TODO: 
+*/
+PPH_ERROR pph_uncock_password_data(pph_context *ctx, uint8 share_number,
+                          const uint8 *usernames[], const uint8 *passwords[]){
+
+  return PPH_ERROR_UNKNOWN;
+}
+ 
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
