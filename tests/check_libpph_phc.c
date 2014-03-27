24af195e6b68761349268b69980264d987bc579d
Santiago Torres <torresariass@gmail.com>
2014-03-27 16:35:51 -0400

    Added the input sanitation test for the phc suite

--- tests/check_libpph_phc.c
+++ tests/check_libpph_phc.c
@@ -14,7 +14,52 @@
 // we will test for extraneous input on the PHS function first
 START_TEST(test_PHS_extraneous_input)
 { 
+  int returnval;
+  uint8 output[DIGEST_LENGTH];
+  uint8 salt[SALT_LENGTH];
+  uint8 password[] = {"testpassword"};
+  // test for null pointers
+  returnval = PHS(output, DIGEST_LENGTH, password, strlen(password), NULL,
+      SALT_LENGTH, 0, 0);
+  ck_assert(returnval == -1);
 
+  returnval = PHS(NULL, DIGEST_LENGTH, password, strlen(password), salt, 
+      SALT_LENGTH, 0,0);
+  ck_assert(returnval == -1);
+
+  returnval = PHS(output, DIGEST_LENGTH, NULL, strlen(password), salt,
+     SALT_LENGTH, 0, 0);
+  ck_assert(returnval == -1);
+  
+  // now let's test for wrong values in the length parameters
+  returnval = PHS(output, DIGEST_LENGTH + 1, password, strlen(password), salt,
+     SALT_LENGTH, 0, 0);
+  ck_assert(returnval == -1);
+
+  returnval = PHS(output, 0, password, strlen(password), salt, SALT_LENGTH, 0,
+     0);
+  ck_assert(returnval == -1);
+
+  returnval = PHS(output, DIGEST_LENGTH, password, 0, salt, SALT_LENGTH, 0, 0);
+  ck_assert(returnval == -1);
+
+  returnval = PHS(output, DIGEST_LENGTH, password, PASSWORD_LENGTH +1, salt,
+     SALT_LENGTH, 0, 0);
+  ck_assert(returnval == -1);
+
+  returnval = PHS(output, DIGEST_LENGTH, password, strlen(password), salt,
+    0, 0, 0);
+  ck_assert(returnval == -1);
+
+  returnval = PHS(output, DIGEST_LENGTH, password, strlen(password), salt,
+     SALT_LENGTH + 1, 0, 0);
+  ck_assert(returnval == -1);
+
+  // lets do a valid generation, we should get a 0 error
+  returnval = PHS(output, DIGEST_LENGTH, password, strlen(password), salt,
+     SALT_LENGTH, 0, 0);
+  ck_assert(returnval == 0); 
+  
 }
 END_TEST
 
